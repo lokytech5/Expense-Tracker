00ee81fc552aaf8519b5912eb0dc3d5cff2efc34
lokytech <lokytech5@gmail.com>
2023-03-11 04:25:13 +0100

    "Add chart and chartBar component"

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import ChartBar from './ChartBar'
 import './Chart.css'
 
-export default function Chart() {
+export default function Chart(props) {
     return (
-        <div>Chart</div>
+        <div className='chart'>
+            {props.dataPoints.map(dataPoint =>
+                <ChartBar
+                    key={dataPoint.label}
+                    value={dataPoint.value}
+                    maxValue={null}
+                    label={dataPoint.label}
+                />)}
+        </div>
     )
 }
