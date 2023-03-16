de69e17462bf1ce7dd8698cbfd3e6b600d1c648f
lokytech <lokytech5@gmail.com>
2023-03-16 22:23:35 +0100

    "build for production"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import { useState } from 'react';
 import Expense from './components/expenses/Expense'
 import NewExpense from './components/NewExpenses/NewExpense';
 
